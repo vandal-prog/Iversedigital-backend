94e2bd01d07cb6881a6d9e2b834652f80a4403a1
afolabidamilare08 <afolabidamilare08@gmail.com>
2025-06-29 15:40:49 +0100

    feat: add getUsersEmail endpoint and deleteProduct route; clean up imports

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Server from 'http';
-// import swaggerDocs from './utils/swagger.js';
 
 import connect from './config/db.js';
 import createApp from './utils/createApp.js';
